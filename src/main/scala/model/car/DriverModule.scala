adb5124cd5509fbf2d628a09cd3a4a9aa3c47ea5
Tiziano Vuksan <tiziano.vuksan@studio.unibo.it>
2025-07-07 23:15:29 +0200

    refactor: scala format

--- src/main/scala/model/car/DriverModule.scala
+++ src/main/scala/model/car/DriverModule.scala
@@ -6,23 +6,27 @@ object DriverModule:
   enum DrivingStyle:
     /** Aggressive driving style: higher speed, faster degradation of the tires and faster fuel consuming. */
     case Aggressive
+
     /** Defensive driving style: slower, prioritizes safety and tire preservation. */
     case Defensive
+
     /** Balanced driving style: average between aggressive and defensive. */
     case Balanced
 
   /** A driver with a name and a driving style. */
   trait Driver:
     /** The name of the driver.
-     *
-     * @return the driver's name
-     */
+      *
+      * @return
+      *   the driver's name
+      */
     def name: String
 
     /** The driving style of the driver.
-     *
-     * @return the driver's [[DrivingStyle]]
-     */
+      *
+      * @return
+      *   the driver's [[DrivingStyle]]
+      */
     def style: DrivingStyle
 
   /** Internal concrete implementation of [[Driver]]. */
@@ -31,10 +35,13 @@ object DriverModule:
   /** Factory for creating [[Driver]] instances. */
   object Driver:
     /** Creates a new driver with the given name and driving style.
-     *
-     * @param name  the name of the driver
-     * @param style the [[DrivingStyle]] the driver adopts
-     * @return a new [[Driver]] instance
-     */
+      *
+      * @param name
+      *   the name of the driver
+      * @param style
+      *   the [[DrivingStyle]] the driver adopts
+      * @return
+      *   a new [[Driver]] instance
+      */
     def apply(name: String, style: DrivingStyle): Driver =
       DriverImpl(name, style)
