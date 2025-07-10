5eed3ed0ab3407754cc4e54044ffa33655448e1d
Tiziano Vuksan <tiziano.vuksan@studio.unibo.it>
2025-07-10 14:17:05 +0200

    docs: fmt format for docs

--- src/main/scala/model/simulation/EventModule.scala
+++ src/main/scala/model/simulation/EventModule.scala
@@ -13,7 +13,7 @@ object EventModule:
   /** Utility functions for events. */
   object Event:
     extension (e: Event)
-      /** @return the string representation of the event*/
+      /** @return the string representation of the event */
       def asString: String = s"Event[+T${e.timestamp}]"
 
   /** A specific type of event only for cars. */
@@ -23,30 +23,34 @@ object EventModule:
   private def validateEvent(timestamp: Double): Unit =
     require(timestamp > 0, "Timestamp needs to be positive.")
 
-  /**
-   * Event representing a car entering a different track sector.
-   *
-   * @param car The car that triggered the event
-   * @param trackSector The track sector that the car entered
-   * @param timestamp The simulation or system time when the event occurred
-   */
+  /** Event representing a car entering a different track sector.
+    *
+    * @param car
+    *   The car that triggered the event
+    * @param trackSector
+    *   The track sector that the car entered
+    * @param timestamp
+    *   The simulation or system time when the event occurred
+    */
   case class TrackSectorEntered(car: Car, trackSector: TrackSector, timestamp: Double) extends CarEvent:
     validateEvent(timestamp)
 
-  /**
-   * Event representing a car exiting its current track sector.
-   *
-   * @param car The car that triggered the event
-   * @param timestamp The simulation or system time when the event occurred
-   */
+  /** Event representing a car exiting its current track sector.
+    *
+    * @param car
+    *   The car that triggered the event
+    * @param timestamp
+    *   The simulation or system time when the event occurred
+    */
   case class TrackSectorExited(car: Car, timestamp: Double) extends CarEvent:
     validateEvent(timestamp)
 
-  /**
-   * Event representing a car requesting a pit stop.
-   *
-   * @param car The car that made the pit stop request
-   * @param timestamp The simulation or system time when the event occurred
-   */
+  /** Event representing a car requesting a pit stop.
+    *
+    * @param car
+    *   The car that made the pit stop request
+    * @param timestamp
+    *   The simulation or system time when the event occurred
+    */
   case class PitStopRequest(car: Car, timestamp: Double) extends CarEvent:
     validateEvent(timestamp)
