830712d2023eb69e075889e15e18f12dddac2602
inesfraccalvierii <ines.fraccalvieri@studio.unibo.it>
2025-08-07 22:14:45 +0200

    refactor: refactor Launcher and add ScalaGPSimulator App

--- src/main/scala/Launcher.scala
+++ src/main/scala/Launcher.scala
@@ -1,22 +1,7 @@
-import app.CarSimulatorApp.stage
-import controller.assembler.SimulationAssembler
-import controller.ui.UISimulationController
-import scalafx.application.JFXApp3
-import view.{SimulationView, StartView}
+import app.ScalaGPSimulator
 
-object Launcher extends JFXApp3:
+/** Launcher of the simulator. */
+object Launcher :
 
-  override def start(): Unit =
-    stage = new JFXApp3.PrimaryStage()
-    showStartPage()
-
-  private def showStartPage(): Unit =
-    StartView.initializeStage(stage, () => launchSimulation())
-
-  private def launchSimulation(): Unit =
-    val assembler = SimulationAssembler()
-    val controller = UISimulationController(assembler)
-    val view = SimulationView(1000, 700, assembler.track)
-    controller.setDisplay(view)
-    view.initializeStage(stage)
-    controller.init()
+  def main(args: Array[String]): Unit =
+    ScalaGPSimulator.main(args)
